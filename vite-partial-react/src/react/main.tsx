982d0e50019fedf3ceea42e6d6bcebc46e676558
Wataru Esaki <74096818+wataruesaki@users.noreply.github.com>
2023-05-04 23:36:24 +0900

    feat: apply a render function to main.tsx

--- vite-partial-react/src/react/main.tsx
+++ vite-partial-react/src/react/main.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { renderReactApp } from './render'
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLDivElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+renderReactApp({
+  elementId: 'root',
+  App: <App />,
+})
